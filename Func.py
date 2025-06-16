54bf9d65da476d67c93f45a9f3cf4cda8b833340
CHE72 <z10.231.195.200@gmail.com>
2025-06-16 23:19:41 +0800

    feat(Func.py): extend public transportation selection platform
    1. 調整 _plan_input() 的邏輯，現在可以回傳月票方案對應的編號與名稱
    2. 大致完成「交通選擇平臺」的骨架，剩餘顯示文字細節需要調整與測試確認
    3. 大致完成最終的計算與輸出邏輯，剩餘文字細節需要調整與測試確認
    4. 尚未開始寫註解

--- Func.py
+++ Func.py
@@ -36,6 +36,7 @@ def _plan_input(city, city_plan):
 
     回傳：
         * plan (int)：
+        * name (str)
 
     拋出：
         * ValueError：
@@ -44,8 +45,9 @@ def _plan_input(city, city_plan):
     print("\n這裡是「方案選擇平臺」，請選擇您想要分析的 \033[38;5;43m{}\033[0m 月票方案".format(TPASS_city[city]))  # 輸出「通勤生活圈」的提示訊息
     print_list(city_plan, 1)  # 呼叫列表印出函式，印出「各城市生活圈」的選單列表
     plan = check_input("--> \033[0m", 1, len(city_plan))  # 呼叫 check_input() 函數讀取與檢查使用者輸入後存放至方案變數，依序傳入 詢問內容、最小數值、最大數值
-    print("\n您選擇了 \033[38;5;43m{}\033[0m 當中的 \033[38;5;43m{}\033[0m 月票方案".format(TPASS_city[city], city_plan[plan - 1]))
-    return plan
+    name = city_plan[plan - 1]
+    print("\n您選擇了 \033[38;5;43m{}\033[0m 當中的 \033[38;5;43m{}\033[0m 月票方案".format(TPASS_city[city], name))
+    return plan, name
 
 
 def check_input(prompt, range_min=None, range_max=None):
@@ -91,7 +93,7 @@ def print_list(content_list, offset=0):  # TODO more Pythonic?
             print("\033[38;5;43m{}: {}".format(i + offset, content_list[i]), end='、')  # 印出編號與列表文字，以頓號分隔元素
 
 
-def analyze(name, ver):
+def analyze(program, ver):
     """
     用於 Main.py 呼叫的選單函數，用於選擇不同的生活圈與具體的月票方案後，呼叫 calculate 函數
     也是本程式的核心分析邏輯部分
@@ -101,134 +103,136 @@ def analyze(name, ver):
         try:
             print("\n這裡是「都會選擇平臺」，請選擇您經常通勤的生活圈")  # 輸出「都會選擇平臺」的提示訊息
             print_list(TPASS_city)  # 呼叫列表印出函式，印出「都會選擇平臺」的選單列表
-            city = check_input("--> \033[0m", 0, len(TPASS_city) - 1)  # 呼叫 check_input() 函數讀取與檢查使用者輸入後存放至時間變數，依序傳入 詢問內容、最小數值、最大數值
+            city = check_input("--> \033[0m", 0, len(TPASS_city) - 1)  # 呼叫 check_input() 函數讀取與檢查使用者輸入後存放至生活圈變數，依序傳入 詢問內容、最小數值、最大數值
 
             match city:
                 case 0:  # 城市0：顯示使用說明
                     pass
                 case 1:  # 城市1：基北北桃生活圈
-                    plan = _plan_input(city, KPPT_plan)
+                    plan, name = _plan_input(city, KPPT_plan)
                     match plan:
                         case 1:  # 月票1：基北北桃跨城際 成人 $1,200
-                            trans = ["結束輸入，開始計算", "臺北捷運", "大臺北地區 市區公車", "新北捷運/輕軌", "臺灣鐵路", "桃園捷運", "桃園/基隆 市區公車", "公路客運", "結束程式運行"]
+                            trans_list = ["結束輸入，開始計算", "臺北捷運+環狀線", "大臺北地區 市區公車", "新北捷運/輕軌", "臺灣鐵路", "桃園捷運", "桃園/基隆 市區公車", "公路客運", "結束程式運行"]
                             price = 1200
-                            bus_full, bus_half = 15, 7
+                            bus_price = [15, 7]
                         case 2:  # 月票2：基北北桃跨城際 學生 $1,200
-                            trans = ["結束輸入，開始計算", "臺北捷運", "大臺北地區 市區公車", "新北捷運/輕軌", "臺灣鐵路", "桃園捷運", "桃園/基隆 市區公車", "公路客運", "結束程式運行"]
+                            trans_list = ["結束輸入，開始計算", "臺北捷運+環狀線", "大臺北地區 市區公車", "新北捷運/輕軌", "臺灣鐵路", "桃園捷運", "桃園/基隆 市區公車", "公路客運", "結束程式運行"]
                             price = 1200
-                            bus_full, bus_half = 12, 6
+                            bus_price = [12, 6]
                         case 3:  # 月票3：基隆市都市內 $288
-                            trans = ["結束輸入，開始計算", "基隆市 市區公車", "臺灣鐵路", "結束程式運行"]
+                            trans_list = ["結束輸入，開始計算", "基隆市 市區公車", "臺灣鐵路", "結束程式運行"]
                             price = 288
 
                 case 2:  # 城市2：桃竹竹苗生活圈
-                    plan = _plan_input(city, TCCM_plan)
+                    plan, name = _plan_input(city, TCCM_plan)
                     match plan:
                         case 1:  # 月票1：桃竹竹苗跨城際 $1,200
-                            trans = ["結束輸入，開始計算", "桃園/新竹/苗栗 市區公車", "桃園捷運", "臺灣鐵路", "公路客運", "結束程式運行"]
+                            trans_list = ["結束輸入，開始計算", "桃園/新竹/苗栗 市區公車", "桃園捷運", "臺灣鐵路", "公路客運", "結束程式運行"]
                             price = 1200
                         case 2:  # 月票2：桃竹竹跨城際 $799
-                            trans = ["結束輸入，開始計算", "桃園/新竹 市區公車", "桃園捷運", "臺灣鐵路", "公路客運", "結束程式運行"]
+                            trans_list = ["結束輸入，開始計算", "桃園/新竹 市區公車", "桃園捷運", "臺灣鐵路", "公路客運", "結束程式運行"]
                             price = 799
                         case 3:  # 月票3：竹竹苗跨城際 $699
-                            trans = ["結束輸入，開始計算", "新竹/苗栗 市區公車", "臺灣鐵路", "公路客運", "結束程式運行"]
+                            trans_list = ["結束輸入，開始計算", "新竹/苗栗 市區公車", "臺灣鐵路", "公路客運", "結束程式運行"]
                             price = 699
                         case 4:  # 月票4：竹竹跨城際 $288
-                            trans = ["結束輸入，開始計算", "新竹（縣/市）市區公車", "臺灣鐵路", "公路客運", "結束程式運行"]
+                            trans_list = ["結束輸入，開始計算", "新竹（縣/市）市區公車", "臺灣鐵路", "公路客運", "結束程式運行"]
                             price = 288
                 case 3:  # 城市3：中彰投苗生活圈
-                    plan = _plan_input(city, CCTM_plan)
+                    plan, name = _plan_input(city, CCTM_plan)
                     match plan:
                         case 1:  # 月票1：中彰投苗跨城際 臺中市民 $699
-                            trans = ["結束輸入，開始計算", "臺中/彰化/南投/苗栗 市區公車", "臺中捷運", "臺灣鐵路", "公路客運", "結束程式運行"]
+                            trans_list = ["結束輸入，開始計算", "臺中/彰化/南投/苗栗 市區公車", "臺中捷運", "臺灣鐵路", "公路客運", "結束程式運行"]
                             price = 699
                         case 2:  # 月票2：中彰投苗跨城際 非臺中市民 $999
-                            trans = ["結束輸入，開始計算", "臺中/彰化/南投/苗栗 市區公車", "臺中捷運", "臺灣鐵路", "公路客運", "結束程式運行"]
+                            trans_list = ["結束輸入，開始計算", "臺中/彰化/南投/苗栗 市區公車", "臺中捷運", "臺灣鐵路", "公路客運", "結束程式運行"]
                             price = 999
                         case 3:  # 月票3：臺中市都市內 臺中市民 $299
-                            trans = ["結束輸入，開始計算", "臺中市 市區公車", "臺中捷運", "臺灣鐵路", "公路客運", "結束程式運行"]
+                            trans_list = ["結束輸入，開始計算", "臺中市 市區公車", "臺中捷運", "臺灣鐵路", "公路客運", "結束程式運行"]
                             price = 299
                         case 4:  # 月票4：臺中市都市內 非臺中市民 $599
-                            trans = ["結束輸入，開始計算", "臺中市 市區公車", "臺中捷運", "臺灣鐵路", "公路客運", "結束程式運行"]
+                            trans_list = ["結束輸入，開始計算", "臺中市 市區公車", "臺中捷運", "臺灣鐵路", "公路客運", "結束程式運行"]
                             price = 599
                         case 5:  # 月票5：彰化縣都市內 $699
-                            trans = ["結束輸入，開始計算", "彰化縣 市區公車", "臺灣鐵路", "公路客運", "結束程式運行"]
+                            trans_list = ["結束輸入，開始計算", "彰化縣 市區公車", "臺灣鐵路", "公路客運", "結束程式運行"]
                             price = 699
                         case 6:  # 月票6：南投縣都市內 $699
-                            trans = ["結束輸入，開始計算", "南投縣 市區公車", "臺灣鐵路", "公路客運", "結束程式運行"]
+                            trans_list = ["結束輸入，開始計算", "南投縣 市區公車", "臺灣鐵路", "公路客運", "結束程式運行"]
                             price = 699
                 case 4:  # 城市4：南高屏生活圈
-                    plan = _plan_input(city, NKP_plan)
+                    plan, name = _plan_input(city, NKP_plan)
                     match plan:
                         case 1:  # 月票1：南高屏跨城際 $999
-                            trans = ["結束輸入，開始計算", "臺南/高雄/屏東 市區公車", "高雄捷運", "高雄輕軌", "高雄渡輪", "臺灣鐵路", "公路客運", "結束程式運行"]  # TODO
+                            trans_list = ["結束輸入，開始計算", "臺南/高雄/屏東 市區公車", "高雄捷運", "高雄輕軌", "高雄渡輪", "臺灣鐵路", "公路客運", "結束程式運行"]  # TODO
                             price = 999
                         case 2:  # 月票2：大臺南公車 $299
-                            trans = ["結束輸入，開始計算", "大臺南公車", "結束程式運行"]  # TODO 僅一種交通選擇，可以簡化
+                            trans_list = ["結束輸入，開始計算", "大臺南公車", "結束程式運行"]  # TODO 僅一種交通選擇，可以簡化
                             price = 299
                         case 3:  # 月票3：大臺南公車+臺鐵 $399
-                            trans = ["結束輸入，開始計算", "大臺南公車", "臺灣鐵路", "結束程式運行"]
+                            trans_list = ["結束輸入，開始計算", "大臺南公車", "臺灣鐵路", "結束程式運行"]
                             price = 399
                         case 4:  # 月票4：高雄市區 $399
-                            trans = ["結束輸入，開始計算", "高雄市 市區公車", "高雄捷運", "高雄輕軌", "高雄渡輪", "臺灣鐵路", "公路客運", "結束程式運行"]  # TODO
+                            trans_list = ["結束輸入，開始計算", "高雄市 市區公車", "高雄捷運", "高雄輕軌", "高雄渡輪", "臺灣鐵路", "公路客運", "結束程式運行"]  # TODO
                             price = 399
                         case 5:  # 月票5：屏東縣公車暢行 $299
-                            trans = ["結束輸入，開始計算", "屏東縣 市區公車", "公路客運", "結束程式運行"]
+                            trans_list = ["結束輸入，開始計算", "屏東縣 市區公車", "公路客運", "結束程式運行"]
                             price = 299
                         case 6:  # 月票6：屏東縣無限暢行 $399
-                            trans = ["結束輸入，開始計算", "屏東縣 市區公車", "臺灣鐵路", "公路客運", "結束程式運行"]
+                            trans_list = ["結束輸入，開始計算", "屏東縣 市區公車", "臺灣鐵路", "公路客運", "結束程式運行"]
                             price = 399
                         case 7:  # 月票7：高雄市公車暢遊 MeNGo（非 TPASS） $199
-                            trans = ["結束輸入，開始計算", "高雄市 市區公車", "結束程式運行"]  # TODO 僅一種交通選擇，可以簡化
+                            trans_list = ["結束輸入，開始計算", "高雄市 市區公車", "結束程式運行"]  # TODO 僅一種交通選擇，可以簡化
                             price = 399
                 case 5:  # 城市5：北宜生活圈
-                    plan = _plan_input(city, BY_plan)
+                    plan, name = _plan_input(city, BY_plan)
                     match plan:
                         case 1:  # 月票1：北宜跨城際及雙北 $2,300  # TODO:UNKNOWN
-                            trans = ["結束輸入，開始計算", "北宜通勤國道客運", "臺灣鐵路", "宜蘭縣 市區公車", "結束程式運行"]
+                            trans_list = ["結束輸入，開始計算", "北宜通勤國道客運", "臺灣鐵路", "臺北捷運", "大臺北地區 市區公車", "宜蘭縣 市區公車", "結束程式運行"]
                             price = 2300
                             continue  # 由於信息不完整，直接跳過本方案的計算，回到「都會選擇平臺」
                         case 2:  # 月票2：北北宜跨城際通勤 $1,800
-                            trans = ["結束輸入，開始計算", "臺灣鐵路", "公路客運", "宜蘭縣 市區公車", "結束程式運行"]
+                            trans_list = ["結束輸入，開始計算", "臺灣鐵路", "公路客運", "宜蘭縣 市區公車", "結束程式運行"]
                             price = 1800
                         case 3:  # 月票3：宜蘭縣縣境內 $750
-                            trans = ["結束輸入，開始計算", "宜蘭縣 市區公車", "臺灣鐵路", "結束程式運行"]
+                            trans_list = ["結束輸入，開始計算", "宜蘭縣 市區公車", "臺灣鐵路", "結束程式運行"]
                             price = 750
                 case 6:  # 城市6：花蓮縣
-                    plan = _plan_input(city, HL_plan)
+                    plan, name = _plan_input(city, HL_plan)
                     match plan:
                         case 1:  # 月票1：花蓮縣都市內 $199
-                            trans = ["結束輸入，開始計算", "花蓮縣 市區公車", "臺灣鐵路", "結束程式運行"]
+                            trans_list = ["結束輸入，開始計算", "花蓮縣 市區公車", "臺灣鐵路", "結束程式運行"]
                             price = 199
                         case 2:  # 月票2：花蓮縣都市內 含公路客運 $399
-                            trans = ["結束輸入，開始計算", "花蓮縣 市區公車", "臺灣鐵路", "公路客運", "結束程式運行"]
+                            trans_list = ["結束輸入，開始計算", "花蓮縣 市區公車", "臺灣鐵路", "公路客運", "結束程式運行"]
                             price = 399
                 case 7:  # 城市7：雲林縣
-                    plan = _plan_input(city, YL_plan)
+                    plan, name = _plan_input(city, YL_plan)
                     match plan:
                         case 1:  # 月票1：雲林縣都市內 $199
-                            trans = ["結束輸入，開始計算", "雲林縣 市區公車", "臺灣鐵路", "公路客運", "結束程式運行"]
+                            trans_list = ["結束輸入，開始計算", "雲林縣 市區公車", "臺灣鐵路", "公路客運", "結束程式運行"]
                             price = 199
                         case 2:  # 月票2：雲林縣都市內 含臺鐵跨區7站 $399
-                            trans = ["結束輸入，開始計算", "雲林縣 市區公車", "臺灣鐵路（含 彰化/嘉義 跨區 7 站）", "公路客運", "結束程式運行"]
+                            trans_list = ["結束輸入，開始計算", "雲林縣 市區公車", "臺灣鐵路（含 彰化/嘉義 跨區 7 站）", "公路客運", "結束程式運行"]
                             price = 399
                 case 8:  # 城市8：澎湖縣
-                    plan = _plan_input(city, PH_plan)
+                    plan, name = _plan_input(city, PH_plan)
                     match plan:
                         case 1:  # 月票1：澎湖縣公車 $210  # TODO 150?
-                            trans = ["結束輸入，開始計算", "澎湖縣 市區公車", "結束程式運行"]  # TODO 僅一種交通選擇，可以簡化
+                            trans_list = ["結束輸入，開始計算", "澎湖縣 市區公車", "結束程式運行"]  # TODO 僅一種交通選擇，可以簡化
                             price = 210
                         case 2:  # 月票2：澎湖縣車船 $1000  #TODO 400?
-                            trans = ["結束輸入，開始計算", "澎湖縣 市區公車", "馬公-望安-七美 交通船", "結束程式運行"]
+                            trans_list = ["結束輸入，開始計算", "澎湖縣 市區公車", "馬公-望安-七美 交通船", "結束程式運行"]
                             price = 1000
                     continue  # 由於信息不完整，直接跳過本方案的計算，回到「都會選擇平臺」
                 case 9:  # 城市9：臺東縣：臺東縣都市內 $299
                     print("\n您選擇了 {} 月票方案".format(TPASS_city[city]))  # 輸出「通勤生活圈」的提示訊息
-                    trans = ["結束輸入，開始計算", "臺東縣 市區公車", "臺灣鐵路", "公路客運", "結束程式運行"]
+                    name = TPASS_city[city]
+                    trans_list = ["結束輸入，開始計算", "臺東縣 市區公車", "臺灣鐵路", "公路客運", "結束程式運行"]
                     price = 299
                 case 10:  # 城市10：大嘉義生活圈：嘉義縣市跨城際 $399
                     print("\n您選擇了 {} 月票方案".format(TPASS_city[city]))  # 輸出「通勤生活圈」的提示訊息
-                    trans = ["結束輸入，開始計算", "嘉義縣/市 市區公車", "臺灣鐵路", "公路客運", "結束程式運行"]
+                    name = TPASS_city[city]
+                    trans_list = ["結束輸入，開始計算", "嘉義縣/市 市區公車", "臺灣鐵路", "公路客運", "結束程式運行"]
                     price = 399
                 case 10:  # 城市11：返回上層選單
                     print("\033[38;5;43m正在返回「功能選擇平臺」\033[0m\n\a")  # 輸出提示訊息與通知聲音
@@ -246,24 +250,95 @@ def analyze(name, ver):
             print("\033[38;5;197m您的輸入內容出現其他錯誤，請檢查後輸入正確選項，現正返回「都會選擇平臺」\033[0m\a\n")  # 輸出提示訊息與通知聲音，讓使用者重新輸入
             continue  # 回到「都會選擇平臺」
 
-        # 直接將 calculate() 的邏輯接在這裡？使用參數 trans(list) price(int)
-        total, result = 0, ""
+        amount, times_list = [0 for t in range(len(trans_list))], [0 for t in range(len(trans_list))]
+        tp_metro_original = 0
+
+        while True:
+            try:
+                print("\n這裡是「交通選擇平臺」，請選擇您要搭乘的大眾運輸工具")  # 輸出「數據輸入平臺」的提示訊息
+                print_list(trans_list)  # 呼叫列表印出函式，印出「都會選擇平臺」的選單列表
+                trans = check_input("--> \033[0m", 0, len(trans_list) - 1)  # 呼叫 check_input() 函數讀取與檢查使用者輸入後存放至交通工具變數，依序傳入 詢問內容、最小數值、最大數值
+
+                match trans_list[trans]:
+                    case "結束輸入，開始計算":
+                        print("calculating")
+                        break
+                    case "結束程式運行":
+                        print("exiting")
+                        sys.exit(0)
+                    case "臺北捷運+環狀線":  # 呼叫 臺北捷運+環狀線 專用輸入函式
+                        print("You are inputing taipei metro")
+                        print("just input 0")
+                        for m in range(20, 70, 5):
+                            times = check_input("NT${} How many times".format(m), 0)
+                            times_list[trans] += times
+                            tp_metro_original += times * m
+                        if 11 <= times_list[trans] <= 20:
+                            amount[trans] = tp_metro_original * 0.95
+                        elif 21 <= times_list[trans] <= 40:
+                            amount[trans] = tp_metro_original * 0.9
+                        elif 41 <= times_list[trans]:
+                            amount[trans] = tp_metro_original * 0.85
+                        else:
+                            amount[trans] = tp_metro_original
+                    case "大臺北地區 市區公車":  # 呼叫 大臺北地區 市區公車 專用輸入函式
+                        print("You are inputing taipei bus")
+                        print("just input 0")
+                        for m in bus_price:
+                            times = check_input("NT${} How many times".format(m), 0)
+                            times_list[trans] += times
+                            amount[trans] += times * m
+                    case _:  # 呼叫 一般輸入函式
+                        print("You are inputing {}".format(trans_list[trans]))
+                        while True:
+                            money = check_input("How much money?", 0)
+                            if money == 0:
+                                break
+                            times = check_input("NT${} How much times_list?".format(money), 0)
+                            if times == 0:
+                                break
+                            times_list[trans] += times
+                            amount += times * money
+
+            except RangeError:  # 如果使用者輸入超出正常範圍的內容
+                print("\033[38;5;197m您的輸入內容超出合理範圍，請檢查後輸入正確內容，現正返回「交通選擇平臺」\033[0m\a\n")  # 輸出提示訊息與通知聲音，讓使用者重新輸入
+                continue  # 回到「交通選擇平臺」
+            except ValueError:  # 如果使用者輸入無法轉換成整數的內容
+                print("\033[38;5;197m您的輸入內容出現非整數的錯誤，請檢查後輸入正確選項，現正返回「交通選擇平臺」\033[0m\a\n")  # 輸出提示訊息與通知聲音，讓使用者重新輸入
+                continue  # 回到「交通選擇平臺」
+            except Exception as e:  # 例外處理，捕捉其他未預期的錯誤
+                print("\033[38;5;197m您的輸入內容出現其他錯誤，請檢查後輸入正確選項，現正返回「交通選擇平臺」\033[0m\a\n")  # 輸出提示訊息與通知聲音，讓使用者重新輸入
+                print(e)  # TODO: remove this after the test is done
+                continue  # 回到「交通選擇平臺」
+
+        # 直接將 calculate() 的邏輯接在這裡？使用參數 trans_list(list) price(int)
+        total, result = 0, "本月共搭乘："
+        print(amount)
+        for m in amount:
+            total += m
+
+        for t in range(1, len(trans_list) - 1):
+            result += "  {}：{}次".format(trans_list[t], times_list[t])  # TODO 頓號
+
         if total < price:
-            result += "本月無須另外購買 TPASS 通勤月票，共計花費 {} 元".format(total)
+            result += "\n本月無須另外購買 TPASS 通勤月票，共計花費 {} 元".format(total)
         elif total == price:
-            result += "本月無論是否購買 TPASS 通勤月票，皆要花費 {} 元".format(price)
+            result += "\n本月無論是否購買 TPASS 通勤月票，皆要花費 {} 元".format(price)
         elif total > price:
-            result += "建議本月購買 {} 方案的 TPASS 通勤月票，可省下 {} 元".format("", total - price)
+            result += "\n建議本月購買「{}」方案的 TPASS 通勤月票，可省下 {} 元".format(name, total - price)
         print(result)
 
 
         # 直接將 output() 的邏輯接在這裡？
         try:
             with open("TPASS_Result.txt", "a+", encoding="UTF-8") as file:
-                time_stamp = time.localtime()
-                file.write("{}-{}-{} {}:{}\n".format(time_stamp.tm_year, time_stamp.tm_mon, time_stamp.tm_mday, time_stamp.tm_hour, time_stamp.tm_min))
+                stamp = time.localtime()
+                file.write("{}-{}-{} {}:{}\n".format(stamp.tm_year, stamp.tm_mon, stamp.tm_mday, stamp.tm_hour, stamp.tm_min))
                 file.write("{}\n".format(result))
-                file.write("「{}」Ver{}\n".format(name, ver))
+                file.write("「{}」Ver{}\n".format(program, ver))
                 file.write("{}\n".format("=" * 36))
         except:
             print("encounter some errors when trying to write \"TPASS_Result.txt\"")
+        else:
+            print("write file successfully")
+            print("going back to city selection")
