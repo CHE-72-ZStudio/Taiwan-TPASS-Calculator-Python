f07658595111ce42a40db37d91ca47cf37dd629c
CHE72 <z10.231.195.200@gmail.com>
2025-07-23 23:30:47 +0800

    fix(Studio.py): change exit code to indicate "Operation not permitted" on direct execution

--- Studio.py
+++ Studio.py
@@ -7,7 +7,7 @@ import sys
 
 if __name__ == "__main__":  # 如果使用者誤啟動本程式
     print("\033[38;5;197m這是用於開源宣告與 ASCII 藝術的模組\n請依照 README.md 的指示，改為運行 Main.py 或其他對應的入口程式，而非直接運行本程式\n我們即將結束此模組的運行\033[0m")  # 輸出提示訊息提醒使用者正確使用方式
-    sys.exit(2)  # 呼叫系統正常結束本程式運行
+    sys.exit(1)  # 呼叫系統結束本程式運行，原因為"Operation not permitted"
 
 Studio_ZH = ("本程式為自由軟體，在自由軟體聯盟發佈的「GNU 通用公共授權合約」的約束下，你可以對其進行再發佈及修改。協議版本為第三版。\n"
              "\033[38;5;43m  CCCCC  H    H  EEEEE       77777  22222         ZZZZZ   SSSS                    d    i         \033[0m\n"
